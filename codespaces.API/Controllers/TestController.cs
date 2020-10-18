3d8bc23d3b00bbb91533bfc06eeb6d0e1b764c36
Mike Brajkovich <mbrajk@users.noreply.github.com>
2020-10-18 21:59:08 +0000

    init a test API to see how sites/ports work

---
+++ codespaces.API/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace codespaces.API.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[]{"a", "b", "c"};
+        }
+    }
+}
